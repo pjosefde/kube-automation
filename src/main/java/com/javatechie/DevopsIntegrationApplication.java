99c03151a0703e4b6ee8e8c18811376aaabbd158
ShubhamBMatere <127843914+ShubhamBMatere@users.noreply.github.com>
2023-07-11 12:12:35 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "Hey hi, I am Shubham Matere";
 	}
 
 	public static void main(String[] args) {
